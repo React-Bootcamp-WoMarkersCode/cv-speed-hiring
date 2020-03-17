7ef8ae3fd034e623cec9b4fb33ca5aaa538fd9b4
=Dayana Maia <=dayrmaia@gmail.com>
2020-03-17 20:32:40 -0300

    pasta de paginas

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div>
+            <h2>Componente campo de busca da lista bootcamps</h2>
+            <h2>Componente Lista bootcamps</h2>
+        </div>
+    )
+}
+
+export default Home;
