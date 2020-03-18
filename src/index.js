f0e85626c91d89dc36df97010e44710d75297290
=Dayana Maia <=dayrmaia@gmail.com>
2020-03-17 22:00:42 -0300

    configurando paper-kit-react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/paper-kit.css";
+// import "assets/css/paper-kit.min.css";
+// import "assets/css/paper-kit.css.map";
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
