7a6afdfecaa2855cda88f85bd26dee3b7ed14a3f
=Dayana Maia <=dayrmaia@gmail.com>
2020-03-18 12:03:32 -0300

    criação das rotas

--- src/Routes.js
+++ src/Routes.js
@@ -6,15 +6,29 @@ const NotFound = React.lazy(() => import('./pages/NotFound'));
 const Routes = () => (
     <Suspense fallback="Loading...">
         <Switch>
-            <Route 
-            exact
-            path='/'
-            component={routerProps => <Home {...routerProps} />} />
- 
-            <Route 
-            exact
-            path='/not-found'
-            component={routerProps => <NotFound {...routerProps} />} />
+            <Route exact path='/' component={routerProps => <Home {...routerProps} />} />
+            
+            <Route exact path='/not-found'component={routerProps => <NotFound {...routerProps} />} />
+            
+            <Route exact path='/solicitar-chave-de-acesso'component={routerProps => <SolicitaChaveAcesso {...routerProps} />} />
+            
+            <Route exact path='/verificar-chave-de-acesso'component={routerProps => <VerificaChaveAcesso {...routerProps} />} />
+            
+            <Route exact path='/participantes/:empresaId'component={routerProps => <Participantes {...routerProps} />} />
+            
+            <Route exact path='/participante/:empresaId/:participanteId'component={routerProps => <Participante {...routerProps} />} />
+            
+            <Route exact path='/cadastrar-conta'component={routerProps => <CadastroConta {...routerProps} />} />
+            
+            <Route exact path='/acessar-conta'component={routerProps => <AcessoConta {...routerProps} />} />
+            
+            <Route exact path='/perfil'component={routerProps => <Perfil {...routerProps} />} />
+            
+            <Route exact path='/cadastrar-evento'component={routerProps => <CadastroEvento {...routerProps} />} />
+            
+            <Route exact path='/politica-de-privacidade'component={routerProps => <PoliticaPrivacidade {...routerProps} />} />
+            
+            <Route exact path='/termo-de-uso'component={routerProps => <TermoUso {...routerProps} />} />
             
             <Redirect to="/not-found" />
         </Switch>
