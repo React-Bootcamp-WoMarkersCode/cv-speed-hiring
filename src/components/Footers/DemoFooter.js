8d5de8e9d24782958690d718b6627e935474541a
Leticia Campos <leticia.campos@totvs.com.br>
2020-03-21 12:12:07 -0300

    retirando comentarios

--- src/components/Footers/DemoFooter.js
+++ src/components/Footers/DemoFooter.js
@@ -1,25 +1,6 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-/*eslint-disable*/
 import React from "react";
 import "./style.css";
-// reactstrap components
+
 import { Row, Container } from "reactstrap";
 
 function DemoFooter() {
@@ -29,7 +10,7 @@ function DemoFooter() {
         <Row>
           <nav className="footer-nav">
             <ul>
-              <li><a href="/sobre">Sobre</a></li>
+              <li>
                 <a href="/sobre">Sobre</a>
               </li>
               <li>
