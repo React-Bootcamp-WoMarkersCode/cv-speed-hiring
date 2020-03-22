352ac352fdb28787edcf1f24090f08381376d69d
annacia <carol.inhasz@hotmail.com>
2020-03-21 22:37:33 -0300

    componente de lista de participantes finalizado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import {Container} from "reactstrap";
 import IndexHeader from "../../components/Headers/IndexHeader";
-import imgHeader from "../../assets//img/grupo-de-pessoas-speed-hiring.jpg";
-import './style.css'
+import imgHeader from "../../assets//img/grupo-de-pessoas-speed-hiring.jpg"
 
 const Home = () => {
     const dataHeader= {
         title: "speed hiring",
         urlImg: imgHeader,
-        description: "Tornando rápido e fácil a busca de novos candidatos, facilitando o processo de contratação."
+        description: "Tornando rápido e fácil a busca de novos candidados,facilitando o processo de contratação."
     }
     return(
         <>
             <IndexHeader props={dataHeader} />
-            <Container>
-                <div className="campo-busca">
-                    <h2>Componente campo de busca da lista bootcamps</h2>
-                </div>
-            </Container>
+            <div>
+                <h2>Componente campo de busca da lista bootcamps</h2>
+                <h2>Componente Lista bootcamps</h2>
+            </div>
         </>
     )
 };
