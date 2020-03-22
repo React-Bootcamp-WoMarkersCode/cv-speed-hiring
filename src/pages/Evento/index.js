e1430dc8c832cc8b92824705f805cd21cc922a50
Leticia Campos <leticia.campos@totvs.com.br>
2020-03-21 23:46:58 -0300

    criação de template com as informações do evento

--- src/pages/Evento/index.js
+++ src/pages/Evento/index.js
@@ -1,19 +1,32 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import { useParams } from 'react-router';
+import { Container, Jumbotron } from 'reactstrap';
 
+import './evento.css';
 import users from '../../data/users'
 
-const Evento = (props) => {
+const Evento = () => {
     const { empresaId } = useParams();
     const eventoList = users[0].eventos;
     const evento = eventoList[empresaId-1];
 
     return(
         <>
-            <div class="container">
-                <img src={evento.img} className="img fluid"></img>
-            </div>
+            <Container>
+                <div className="flexivel">
+                    <img src={evento.img} className="col-md-6"></img>
+                    <Jumbotron>
+                        <h3><b>{evento.nomeEvento}</b></h3>
+                        <hr className="my-2" />
+                        <p>{evento.descricao}</p>
+                        <hr className="my-2" />
+                        <p><b>Categoria: </b>{evento.categoria}</p>
+                        <p><b>Início: </b>{evento.dataInicio}</p>
+                        <p><b>Conclusão: </b>{evento.dataFim}</p>
+                    </Jumbotron>
+                </div>
+            </Container>
         </>
     )
 }
