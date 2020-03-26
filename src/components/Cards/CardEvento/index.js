92dd73e647c4cd97cc5317ba20413503e0fe142c
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-03-26 13:30:30 -0300

    resolvendo conflitos de merge

---
+++ src/components/Cards/CardEvento/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+    Card,
+    Button,
+    CardImg,
+    CardTitle,
+    CardText,
+    CardDeck,
+    CardSubtitle,
+    CardBody
+} from 'reactstrap';
+
+import './style.css'
+
+const CardEvento = (props) => {
+
+    const { evento } = props
+
+    return (
+        <>
+            <CardDeck className="card-deck">
+                <Card>
+                    <CardImg top width="100%" src={evento.img} alt="Banner com a imagem do workshop/bootcamp" />
+                    <CardBody>
+                        <CardTitle className="title">{evento.nomeEvento}</CardTitle>
+                        <CardSubtitle className="sub-titulo">WoMakersCode</CardSubtitle>
+                        <CardText>{evento.categoria}</CardText>
+                        <CardText>{evento.descricao}</CardText>
+                        <Link to={`/evento/${evento.id}`}>
+                            <Button>Saber mais</Button>
+                        </Link>
+                    </CardBody>
+                </Card>
+            </CardDeck>
+        </>
+    );
+};
+
+export default CardEvento;
