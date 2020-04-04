0c0d52c430f1057bdaf0376d91398e3fde1b8baf
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-04 14:05:34 -0300

    atualizando as dependencias

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,5 +5,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
 
 configure({ adapter: new Adapter() });
