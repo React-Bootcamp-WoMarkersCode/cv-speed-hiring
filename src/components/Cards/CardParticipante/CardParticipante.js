fb749c541617dae9f1996dbd5dd50e41c8fcd639
Ariane Gomes de Sousa <ariane.sousa011@gmail.com>
2020-04-06 18:50:07 -0300

    Alteração do id

--- src/components/Cards/CardParticipante/CardParticipante.js
+++ src/components/Cards/CardParticipante/CardParticipante.js
@@ -7,14 +7,19 @@ import './styles.css';
 
 const CardParticipante = (props) => {
 
-  const { nome, avatar, cargo, linkedin } = props
+  const { nome, avatar, cargo, key } = props
+  // const { empresaId } = useParams();
+  // const eventoList = users[0].eventos;
+  // const evento = eventoList[empresaId-1];
+  
+  // const participantes = evento.participantes;
 
   return (
     <div id="card_participante_conteudo">
       <Card id="card_participante">
         <div className="card_participante-body">
           <AvatarParticipante nome={nome} avatar={avatar} />
-          <IdentidadeParticipante nome={nome} cargo={cargo} linkedin={linkedin}/>
+          <IdentidadeParticipante nome={nome} cargo={cargo} id={key}/>
         </div>
       </Card>
     </div>
