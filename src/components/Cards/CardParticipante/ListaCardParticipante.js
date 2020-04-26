ed2ec1f98bf905139c41d5da1e0c5b27188191bd
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-26 11:42:38 -0300

    impl loading na lista de participantes

--- src/components/Cards/CardParticipante/ListaCardParticipante.js
+++ src/components/Cards/CardParticipante/ListaCardParticipante.js
@@ -1,21 +1,47 @@
 import React, {useEffect, useState} from 'react';
+import FadeIn from "react-fade-in";
+import Lottie from "react-lottie";
 import { CardDeck } from 'reactstrap';
 import CardParticipante from './CardParticipante';
+import * as quotesloading from '../../../assets/json/quotesloading.json';
 import './styles.css';
+
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: quotesloading.default,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    }
+}
 
 const ListaCardParticipante = (props) => {
 
     const { participantes } = props;
     const [changAarray, setChangAarray] = useState([]);
+    const [ loading, setLoading ] = useState(false);
 
-    const listaParticipantes = changAarray.map((p, index) => 
-        <CardParticipante key={index}
-            nome={p.nome}
-            cargo={p.cargo}
-            avatar={p.avatar}
-            idParticipante={p.key}
-        />
-    );
+    let listaParticipantes = [];
+
+    if (loading) {
+        listaParticipantes = changAarray.map((p, index) => 
+            <CardParticipante key={index}
+                nome={p.nome}
+                cargo={p.cargo}
+                avatar={p.avatar}
+                idParticipante={p.key}
+            />
+        );
+    } else {
+        listaParticipantes =
+            <FadeIn>
+                <div className="d-flex justify-content-center align-items-center mt-5">
+                    <h4>Carregando a lista de participantes...</h4>
+                </div>
+                <Lottie options={defaultOptions} height={500} width={500} />
+            </FadeIn>
+    }
 
     useEffect(() => {
         if(participantes) {
@@ -27,6 +53,7 @@ const ListaCardParticipante = (props) => {
             });
 
             setChangAarray(items);
+            setLoading(true);
         }
     }, [participantes])
 
