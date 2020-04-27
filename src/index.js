4b1c44297f2f213cbbf231ccd8d9144e27521f0d
Ariane Gomes de Sousa <ariane.sousa011@gmail.com>
2020-04-26 23:06:39 -0300

    Aplicação de layout e identidade visual na page de cadastro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import "./assets/css/paper-kit.css";
 import './index.css';
